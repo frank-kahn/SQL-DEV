04b0e92ef6bbddbdc398877329c15eb52808f5d1
yk <64@example.com>
2024-02-21 07:59:45 +0800

    add data

--- scripts/2、Oracle/权限管理.sql
+++ scripts/2、Oracle/权限管理.sql
@@ -1,3 +1,5 @@
+-- 创建用户
+create user yaokang identified by yaokang;
 -- 给用户授权所有数据字典的查询权限
 grant select any dictionary to test_user;
 
@@ -5,3 +7,42 @@ grant select any dictionary to test_user;
 grant datapump_exp_full_database to test01;
 
 -- 获取某个用户的全部权限：系统权限、对象权限
+-- 对象权限
+select OWNER,TABLE_NAME,GRANTOR,PRIVILEGE from user_tab_privs where GRANTEE='TEST_USER';
+
+
+
+
+-- 查看用户下拥有的表
+select OWNER,TABLE_NAME,TABLESPACE_NAME from all_tables where OWNER='TEST_USER';
+
+
+-- 批量授权
+create or replace procedure grantqx(v_from in varchar2, v_to in varchar2) is
+v_sql varchar2(1000);
+cursor v_cur is
+select t.* from dba_tables t where t.OWNER = v_from;
+begin
+for v_row in v_cur loop
+v_sql := 'grant select on ' || v_from || '.' || v_row.table_name ||' to ' || v_to;
+execute immediate v_sql;
+end loop;
+end grantqx;
+
+
+DECLARE
+v_sql varchar2(1000);
+cursor v_cur is
+select t.* from dba_tables t where t.OWNER = 'TEST_USER';
+begin
+for v_row in v_cur loop
+v_sql := 'grant select on ' || 'TEST_USER' || '.' || v_row.table_name ||' to ' || 'YAOKANG';
+DBMS_OUTPUT.ENABLE;
+DBMS_OUTPUT.PUT_LINE(v_sql);
+-- execute immediate v_sql;
+end loop;
+end;
+/
+
+
+select OWNER,TABLE_NAME,GRANTOR,PRIVILEGE from user_tab_privs where regexp_like(TABLE_NAME,'^test_t','i');

6b9ed7faeb03198eb036eae312aadd2d98d26524
yk <64@example.com>
2024-03-05 08:09:10 +0800

    add data

--- scripts/3、MySQL/2、权限管理.sql
+++ scripts/3、MySQL/2、权限管理.sql
@@ -1,3 +1,13 @@
 -- 备份MySQL用户的密码
 select concat('alter user \'',user,'\'@\'',host,'\' identified with \'mysql_native_password\' as ','\'',authentication_string,'\';') from mysql.user;
 
+-- mysql 密码验证
+-- 5.7版本
+select host,user,plugin,authentication_string,password('rootroot') from mysql.user where user='root';
+-- 8.0版本
+select host,user,plugin,authentication_string,sha('rootroot') from mysql.user where user='root';
+
+
+
+-- 刷权限
+flush privileges;
